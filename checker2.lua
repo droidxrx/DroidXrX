7419e0b6368fdd6d2bf6b82cf7e6284b5ac04c36
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-12-27 11:11:11 +0700

    Create checker2.lua

---
+++ checker2.lua
@@ -0,0 +1,15 @@
+function pront()print("New Version Available\nPlease Update Script Link Download On My Telegram\nJoin Telegram: https://t.me/droidtamvanreal")end
+data = true
+if data == false then
+  local menu = gg.alert('New Version Available\nPlease Update Script Link Download On My Telegram\nJoin Telegram: https://t.me/droidtamvanreal', 'Copy Link Telegram', 'Exit')
+  if menu == 0 then
+    pront() error() os.exit()
+  elseif menu == 1 then
+    gg.copyText('https://t.me/droidtamvanreal') pront() error() os.exit()
+  elseif menu == 2 then
+    pront() error() os.exit()
+  end
+  pront()
+  error()
+  os.exit()
+end
