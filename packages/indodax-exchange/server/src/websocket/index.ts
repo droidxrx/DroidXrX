165770ce8e7f95e00bcc267d41490e3778f24b84
DroidXrX <droidxrx@gmail.com>
2021-10-11 01:09:39 +0700

    Add Privare Project

--- packages/indodax-exchange/server/src/websocket/index.ts
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer } from 'ws';
-import { Server } from 'http';
-import initSocket from './initSocket';
-
-export default (expressServer: Server) => {
-	const socketInit = initSocket(expressServer, WebSocketServer);
-	console.log(`List websocket url:\n${socketInit.socketsUrl}`);
-};
