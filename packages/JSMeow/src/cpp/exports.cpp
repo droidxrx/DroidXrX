9c1e79fb6087b79c5d93e091641d1f4c13a9db54
DroidXrX <droidxrx@gmail.com>
2022-03-20 13:51:35 +0700

    Assign object

--- packages/JSMeow/src/cpp/exports.cpp
+++ packages/JSMeow/src/cpp/exports.cpp
@@ -2,11 +2,24 @@
 #include "misc.h"
 #include "overlay.h"
 
-Object Inits(Env env, Object exports) {
-	ovInit(env, exports);
-	drawsInit(env, exports);
-	miscInit(env, exports);
+Object assignObject(Env env, Object exports) {
+	Object overlay = Object::New(env);
+	Object draws = Object::New(env);
+	Object misc = Object::New(env);
 
+	ovInit(env, overlay);
+	drawsInit(env, draws);
+	miscInit(env, misc);
+
+	exports.Set("overlay", overlay);
+	exports.Set("draws", draws);
+	exports.Set("misc", misc);
+
+	return exports;
+}
+
+Object Inits(Env env, Object exports) {
+	assignObject(env, exports);
 	return exports;
 }
 
