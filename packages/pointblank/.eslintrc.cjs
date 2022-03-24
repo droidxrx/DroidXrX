c1d9969bb101bf92df186ce8044c579cfb38b584
DroidXrX <droidxrx@gmail.com>
2022-03-24 09:33:41 +0700

    removed

--- packages/pointblank/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	extends: ['@nodejs/eslint-config-all/ts'],
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ['tsconfig.json'],
-	},
-	ignorePatterns: ['*.js', '*.cjs', '*.d.ts'],
-};
--- packages/pointblank/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { Overlay, Misc, utils } from '@nodejs/hacktool';
-
-const overlay = Overlay.init('FullScreen', 0x23, 25);
-console.log(overlay);
